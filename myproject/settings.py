9adb0347c0133b4bcbdbbd5daa1c380e84071602
tobe <tobemarizu@gmail.com>
2025-01-28 07:42:35 +0100

    host

--- myproject/settings.py
+++ myproject/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-z603e(i6noio20z-qh2h9=y!_%okgt$p2+^mpsyoz%*0ps*+ss'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://malzweb.onrender.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
